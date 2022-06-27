ef39cd8de2eacaa49c003c1d618c57e0e5fcc007
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-06-27 10:09:19 +0530

    Create unary.c
    27-06-2022 Q2

---
+++ unary.c
@@ -0,0 +1,21 @@
+// 27-06-2022 Q2
+
+#include <stdio.h>
+int main()
+{
+        int a=5, b=10, c=15;
+        printf("%d\n", a++);
+        printf("%d\n", a--);
+        printf("%d\n", ++a);
+        printf("%d\n", --a);
+        printf("%d\n", b++);
+        printf("%d\n", b--);
+        printf("%d\n", ++b);
+        printf("%d\n", --b);
+        printf("%d\n", c++);
+        printf("%d\n", c--);
+        printf("%d\n", ++c);
+        printf("%d\n", --c);
+
+        return 0;
+}
