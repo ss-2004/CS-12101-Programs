842dc1f187cbb52768a836fee315bda330225f5c
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-06-27 10:05:15 +0530

    Create printSizes.c

---
+++ printSizes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        printf("int : %d\n", sizeof(int));
+        printf("float : %d\n", sizeof(float));
+        printf("char : %d\n", sizeof(char));
+        printf("double : %d\n", sizeof(double));
+        printf("short int : %d\n", sizeof(short int));
+        printf("long double : %d\n", sizeof(long double));
+
+        return 0;
+}
