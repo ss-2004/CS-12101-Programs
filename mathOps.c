464fbb70498e95aa6c9b9c9425faccc608e79b23
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-06-27 10:29:45 +0530

    Create mathOps.c
    13-06-2022 Q2

---
+++ mathOps.c
@@ -0,0 +1,16 @@
+// 13-06-2022 Q2
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	printf("Enter a, b : ");
+	scanf("%d %d", &a, &b);
+	printf("Sum : %d \n", (a+b));
+ 	printf("Dif : %d \n", (a-b));
+	printf("Mul : %d \n", (a*b));
+	printf("Div : %d \n", (a/b));
+
+	return 0;
+}
