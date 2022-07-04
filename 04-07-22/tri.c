8497272bcba4c04542873236dae9379b2f478444
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-04 10:50:31 +0530

    Q5

---
+++ 04-07-22/tri.c
@@ -0,0 +1,17 @@
+// 04-07-2022
+
+#include <stdio.h>
+
+int main()
+{
+	int m=0, n=0;
+	printf("Enter m : ");
+	scanf("%d", &m);
+	if(m>0)
+		n = 1;
+	else if(m<0)
+		n = -1;
+
+	printf("n = %d\n", n);
+	return 0;
+}
