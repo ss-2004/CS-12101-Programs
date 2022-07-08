3e6f7c814e89d5a731590e6722b0faa17ead5633
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-09 00:44:20 +0530

    Q4

--- 04-07-22/grade.c
+++ 04-07-22/grade.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-	int m=0, n=0;
-	printf("Enter m : ");
-	scanf("%d", &m);
-	if(m>0)
-		n = 1;
-	else if(m<0)
-		n = -1;
-
-	printf("n = %d\n", n);
+	int marks;
+	printf("Enter marks : ");
+	scanf("%d", &marks);
+	if(marks < 50) printf("Grade : F\n");
+	else if(50 <= marks && marks < 60) printf("Grade : C\n");
+	else if(60 <= marks && marks < 70) printf("Grade : B\n");
+	else if(70 <= marks && marks < 80) printf("Grade : B+\n");
+	else if(80 <= marks && marks < 90) printf("Grade : A\n");
+	else if(90 <= marks && marks <= 100) printf("Grade : A+\n");
 	return 0;
 }
