2344754dcf31e9323b432f41149e190f0e9e5bff
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-09 01:03:50 +0530

    Q6

--- 04-07-22/relation.c
+++ 04-07-22/relation.c
@@ -4,14 +4,18 @@
 
 int main()
 {
-	int m=0, n=0;
-	printf("Enter m : ");
-	scanf("%d", &m);
-	if(m>0)
-		n = 1;
-	else if(m<0)
-		n = -1;
-
-	printf("n = %d\n", n);
+	int a,b;
+	printf("Enter a : ");
+	scanf("%d", &a);
+	printf("Enter b : ");
+	scanf("%d", &b);
+	if(a == b) printf("%d is equal to %d\n", a, b);
+	else printf("%d is not equal to %d\n", a, b);
+	
+	if(a > b) printf("%d is greater to %d\n", a, b);
+	else printf("%d is greater to %d\n", a, b);
+	
+	if(a < b) printf("%d is smaller to %d\n", a, b);
+	else printf("%d is smaller to %d\n", a, b);
 	return 0;
 }
