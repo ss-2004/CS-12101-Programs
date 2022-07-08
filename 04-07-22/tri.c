7426089d5788c06db9e03d05cd91be5959726238
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-09 00:48:37 +0530

    Q5

--- 04-07-22/tri.c
+++ 04-07-22/tri.c
@@ -4,14 +4,12 @@
 
 int main()
 {
-	int m=0, n=0;
-	printf("Enter m : ");
-	scanf("%d", &m);
-	if(m>0)
-		n = 1;
-	else if(m<0)
-		n = -1;
-
-	printf("n = %d\n", n);
+	int angle1, angle2, angle3;
+	printf("Enter angles : ");
+	scanf("%d %d %d", &angle1, &angle2, &angle3);
+	if(angle1+angle2+angle3 == 180)
+		printf("VALID TRIANGLE");
+	else
+		printf("INVALID TRIANGLE");
 	return 0;
 }
