16488ba33229dfa3d64ada54ce36449698c65070
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-11 23:28:52 +0530

    Q9

--- 11-07-22/pattern9.c
+++ 11-07-22/pattern9.c
@@ -1,1 +1,18 @@
-//placeholder
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+    for (i=1; i<=3; i++)
+    {
+        for(j=1; j<=3-i; j++) printf(" ");
+        for(j=1; j<=2*i-1; j++) printf("*");
+        printf("\n");
+    }
+
+    for (i=1; i<3; i++)
+    {
+        for(j=1; j<=i; j++) printf(" ");
+        for(j=1; j<=2*(3-i)-1; j++) printf("*");
+        printf("\n");
+    }
+}
