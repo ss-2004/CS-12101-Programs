dcbb484a233a679bd7407afbff7069e74d658a89
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-07-11 12:20:29 +0530

    Q7

--- 11-07-22/pattern7.c
+++ 11-07-22/pattern7.c
@@ -1,1 +1,29 @@
-//placeholder
+#include <stdio.h>
+int main()
+{
+	int i, j, k, l=1;
+	
+	for(i=7; i>=1; i--)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf(" %c ", (char)(64+j));			
+		}
+		if(i<=6)
+		{
+			printf(" ");
+			if(i<=5)
+				for(k=1; k<=l; k++)printf("  ");	
+			else
+				printf("  ");
+			for(k=j-1; k>=1; k--)printf(" %c ", (char)(64+k));
+			l+=3;
+		}
+		else
+			for(k=j-2; k>=1; k--)printf(" %c ", (char)(64+k));
+		printf("\n"); 
+	}
+	
+	return 0;
+}
+
